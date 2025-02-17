06a898b033be7033c3a43650cd601670b8acec4e
Frederic Bec <bec.frederic90@outlook.fr>
2025-02-17 14:15:34 +0100

    Add class QuestionAdmin and ChoiceAdmin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date"]
+    list_filter = ["question_text", "pub_date"]
+    ordering = ["pub_date"]
+    search_fields = ["question_text", "pub_date"]
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ["choice_text", "question", "votes"]
+    list_filter = ["choice_text", "question", "votes"]
+    ordering = ["votes"]
+    search_fields = ["choice_text", "question__question_text", "votes"]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
