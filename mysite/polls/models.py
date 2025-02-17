e77856d3c01010fe3ea36d6e405bff80435b13bb
Frederic Bec <bec.frederic90@outlook.fr>
2025-02-17 10:57:05 +0100

    Add toString and custom method

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,6 @@
+import datetime
+from django.utils import timezone
+
 from django.db import models
 
 
@@ -6,9 +9,22 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        """
+        Determinate if the question was published recently
+        :return: boolean
+        """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     """Choice of question"""
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
