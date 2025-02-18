7a44ebfaaa258110814ee245dd7c6fc8659b47d2
Frederic Bec <bec.frederic90@outlook.fr>
2025-02-18 09:14:26 +0100

    Add new views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,37 @@
 from django.http import HttpResponse
 
+
 def index(request):
+    """home view"""
     return HttpResponse("Hello, world. You're at the polls index")
+
+
+def detail(request, question_id):
+    """
+    Question's detail
+    :param request:
+    :param question_id: id of the question
+    :return: question's detail view
+    """
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    """
+    View of question's results
+    :param request:
+    :param question_id:
+    :return:
+    """
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    """
+    View of votes
+    :param request:
+    :param question_id:
+    :return:
+    """
+    return HttpResponse("You're voting on question %s." % question_id)
