faf1333e7126b0184469bbf6366fdf8f1616d369
Frederic Bec <bec.frederic90@outlook.fr>
2025-02-18 09:30:23 +0100

    update views and add templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 
 def index(request):
-    """home view"""
-    return HttpResponse("Hello, world. You're at the polls index")
+    """
+    home view
+    :param request: request
+    :return: question list view
+    """
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
@@ -13,7 +24,8 @@ def detail(request, question_id):
     :param question_id: id of the question
     :return: question's detail view
     """
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
