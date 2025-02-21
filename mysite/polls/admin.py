c44933f8fdadd81f50c5265207954e0540d5eb1e
Frederic Bec <bec.frederic90@outlook.fr>
2025-02-21 10:52:31 +0100

    update admin models

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-from .models import Question, Choice
+from .form import CourseAdminForm
+from .models import Question, Choice, Learner, Teacher, Course
 
 
 class ChoiceInline(admin.TabularInline):
@@ -28,5 +31,33 @@ class ChoiceAdmin(admin.ModelAdmin):
     search_fields = ["choice_text", "question__question_text", "votes"]
 
 
+class CourseAdmin(admin.ModelAdmin):
+    form = CourseAdminForm
+    list_display = ["name", "teacher", "start_date", "end_date"]
+    list_filter = ["name"]
+    ordering = ["name", "start_date", "end_date"]
+    search_fields = ["name"]
+    filter_vertical = ["learners"]
+
+
+class LeanerInline(admin.StackedInline):
+    model = Learner
+    can_delete = False
+    verbose_name_plural = "learner"
+
+
+class TeacherInline(admin.StackedInline):
+    model = Teacher
+    can_delete = False
+    verbose_name_plural = "teacher"
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [LeanerInline, TeacherInline]
+
+
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- mysite/polls/form.py
+++ mysite/polls/form.py
@@ -1,5 +1,16 @@
 from django import forms
 
+from .models import Course, Learner
+
 
 class PollForm(forms.Form):
     question_text = forms.CharField(label="Question", max_length=100)
+
+class CourseAdminForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+    learners = forms.ModelMultipleChoiceField(
+        queryset=Learner.objects.all(), required=False  # Champ devient facultatif
+    )
