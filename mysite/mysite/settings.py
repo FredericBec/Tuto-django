6f10b7a4f24cd0ca8d463562690f6d8a2eecda45
Frederic Bec <bec.frederic90@outlook.fr>
2025-05-20 14:07:35 +0200

    modify jwt settings and requirements

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
