ded86c4676af6fc60a9efa4c3a6ff8c2812fbf15
Frederic Bec <bec.frederic90@outlook.fr>
2025-05-20 14:01:56 +0200

    install jwt authentication

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,6 +2,7 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from polls.api_views import QuestionViewset, ChoiceViewset, AdminQuestionViewset, AdminChoiceViewset
 
@@ -15,5 +16,7 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include(router.urls))
 ] + debug_toolbar_urls()
